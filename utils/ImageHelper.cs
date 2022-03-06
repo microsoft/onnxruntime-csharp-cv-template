bb1454a51a722e293a918d2b5a25abda864f9f74
SkywalkerIsNull <ashley@skywalker.net.au>
2022-03-06 16:04:45 +1000

    Slightly faster again.

--- utils/ImageHelper.cs
+++ utils/ImageHelper.cs
@@ -32,14 +32,21 @@ namespace OnnxRuntime.ResNet.Template
                 });
             });
 
-            var images = new List<Image<Rgb24>>() {image};
+
+            var images = new List<Image<Rgb24>>() { image };
+
             var mean = new[] { 0.485f, 0.456f, 0.406f };
             var stddev = new[] { 0.229f, 0.224f, 0.225f };
 
             // Shape of the input tensor to the OnnxRuntime model
             var inputDimensions = new int[] {1, 3, 224, 224};
 
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             var tensors = ImageToTensor(images, mean, stddev, inputDimensions);
+            Console.WriteLine($"Time to create batches: {stopwatch.Elapsed}");
+            Console.WriteLine();
 
             return tensors;
         }
@@ -60,12 +67,14 @@ namespace OnnxRuntime.ResNet.Template
             // If required, can create batches of different sizes
             var batchSizes = new int[] {images.Count};
 
-            // Keep track of which tile we are using to create multiple batches.
-            int tileIndex = 0;
-
             var strides = GetStrides(inputDimension);
 
             var inputs = new List<DenseTensor<float>>();
+
+            // Faster normalisation process
+            var normR = mean[0] / stddev[0];
+            var normG = mean[1] / stddev[1];
+            var normB = mean[2] / stddev[2];
 
             for (var j = 0; j < numberBatches; j++)
             {
@@ -75,45 +84,38 @@ namespace OnnxRuntime.ResNet.Template
                 // Need to directly use a DenseTensor here because we need access to the underlying span.
                 DenseTensor<float> input = new DenseTensor<float>(inputDimension);
 
-                // This is the index used to get the stride offsets for the span.
-                // mdIndex[0] = row in the batch.
-                // mdIndex[1] = either 1/2/3 depending on if its for RBG
-                // mdIndex[2] = y value corresponding to the current image height
-                // mdIndex[3] = Always 0 since we want the start index of the x values
-                int[] mdIndex = new int[4];
-                mdIndex[3] = 0;
                 for (var i = 0; i < batchSizes[j]; i++)
                 {
-                    mdIndex[0] = i;
-                    var image = images[tileIndex];
+                    var image = images[i];
+                    var index = 0;
+
                     image.ProcessPixelRows(pixelAccessor =>
                     {
                         var inputSpan = input.Buffer.Span;
                         for (var y = 0; y < image.Height; y++)
                         {
-                            mdIndex[2] = y;
+                            index = y * strides[2];
 
                             var rowSpan = pixelAccessor.GetRowSpan(y);
 
-                            // Update the mdIndex based on R/G/B and get a span to the underlying memory
-                            mdIndex[1] = 0;
-                            var spanR = inputSpan.Slice(GetIndex(strides, mdIndex), image.Width);
-                            mdIndex[1] = 1;
-                            var spanG = inputSpan.Slice(GetIndex(strides, mdIndex), image.Width);
-                            mdIndex[1] = 2;
-                            var spanB = inputSpan.Slice(GetIndex(strides, mdIndex), image.Width);
+                            // Faster indexing into the span
+                            var spanR = inputSpan.Slice(index, image.Width);
+                            index += strides[1];
+                            var spanG = inputSpan.Slice(index, image.Width);
+                            index += strides[1];
+                            var spanB = inputSpan.Slice(index, image.Width);
+                            index += strides[1];
 
                             // Now we can just directly loop through and copy the values directly from span to span.
                             for (int x = 0; x < image.Width; x++)
                             {
-                                spanR[x] = ((rowSpan[x].R / 255f) - mean[0]) / stddev[0];
-                                spanG[x] = ((rowSpan[x].G / 255f) - mean[1]) / stddev[1];
-                                spanB[x] = ((rowSpan[x].B / 255f) - mean[2]) / stddev[2];
+                                spanR[x] = (rowSpan[x].R / (255f * stddev[0])) - normR;
+                                spanG[x] = (rowSpan[x].G / (255f * stddev[1])) - normG;
+                                spanB[x] = (rowSpan[x].B / (255f * stddev[2])) - normB;
                             }
                         }
                     });
-
-                    tileIndex++;
+                    
                     inputs.Add(input);
                 }
             }
